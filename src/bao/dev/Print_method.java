180dba51deaf67ccaf52ba16b05defda3e6e70e8
bao <camhoangbao@gmail.com>
2018-09-20 16:49:54 +0700

    Finish Print_method

---
+++ src/bao/dev/Print_method.java
@@ -0,0 +1,20 @@
+package bao.dev;
+
+	import java.io.BufferedReader;
+	import java.io.FileInputStream;
+	import java.io.InputStreamReader;
+
+	public class Print_method {
+	   public static void main (String[]args) {
+			try {
+				FileInputStream fis = new FileInputStream("in.txt");
+				BufferedReader br = new BufferedReader (new InputStreamReader(fis));
+				String line ;
+			while ((line = br.readLine()) !=null) {
+					System.out.println(line);
+				}
+				br.close ();
+			} catch (Exception e) {	
+			}		
+		}
+}
